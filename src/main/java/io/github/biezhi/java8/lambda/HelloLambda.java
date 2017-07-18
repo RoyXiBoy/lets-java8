1f7d28d152bb9a30a9363a6e0f6f0e46466410ca
biezhi <biezhi.me@gmail.com>
2017-07-18 11:40:34 +0800

    🥔

--- src/main/java/io/github/biezhi/java8/lambda/HelloLambda.java
+++ src/main/java/io/github/biezhi/java8/lambda/HelloLambda.java
@@ -3,11 +3,22 @@ package io.github.biezhi.java8.lambda;
 /**
  * 第一个Lambda表达式程序
  * <p>
+ *
+ * @see Runnable
  * Created by biezhi on 2017/7/17.
  */
 public class HelloLambda {
 
     public static void main(String[] args) {
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("do something.");
+            }
+        };
+
+        // 使用lambda表达式创建一个Runnable对象
         Runnable runnable = () -> System.out.println("Hello::" + Thread.currentThread());
         new Thread(runnable).start();
     }
